b6caa20276c088cac2627808a4d0b183dce73694
ICSF2442 <icsf2442@gmail.com>
2023-06-29 01:05:53 +0100

    Backend 100% concluido

--- api/v1/calculus/removeCalculus.php
+++ api/v1/calculus/removeCalculus.php
@@ -1,1 +1,29 @@
 <?php
+
+require_once('./../../settings.php');
+
+use Functions\Database;
+
+use Functions\Utils;
+use Objects\RequestResponse;
+use Objects\User;
+
+$request = new RequestResponse();
+$json = Utils::getRequestBody();
+
+if ($json == null) {
+    echo "ERRO! JSON INVALIDO!";
+
+} else {
+    $id = null;
+    if ($json["id"] != null) {
+        $id = $json["id"];
+        \Objects\Calculus::remover($id);
+        (new RequestResponse())->setResult("Calculus removido.")->response();
+    }else{
+        $request->setError("Erro!");
+        $request->setIsError(true);
+        echo($request->response(false));
+    }
+
+}
--- api/v1/calculus/removeCandidate.php
+++ api/v1/calculus/removeCandidate.php
@@ -1,1 +1,30 @@
 <?php
+
+
+require_once('./../../settings.php');
+
+use Functions\Database;
+
+use Functions\Utils;
+use Objects\RequestResponse;
+use Objects\User;
+
+$request = new RequestResponse();
+$json = Utils::getRequestBody();
+
+if ($json == null) {
+    echo "ERRO! JSON INVALIDO!";
+
+} else {
+    $id = null;
+    if ($json["id"] != null) {
+        $id = $json["id"];
+        \Objects\Candidate::remover($id);
+        (new RequestResponse())->setResult("Candidato removido.")->response();
+    } else {
+        $request->setError("Erro!");
+        $request->setIsError(true);
+        echo($request->response(false));
+    }
+
+}
